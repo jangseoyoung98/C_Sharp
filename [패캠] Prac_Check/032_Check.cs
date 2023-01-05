51c626a60fd76fb9872a7005f404e83a71433731
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-01-05 22:37:34 +0900

    032_Check

---
+++ [패캠] Prac_Check/032_Check.cs
@@ -0,0 +1,23 @@
+﻿// GetType() 함수로 (int, string, float, char)형 CTS 표준 출력하기
+using System;
+
+namespace _032_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            string b = "AAA";
+            Single c = 0.123f; // Single 구조체 - 4바이트 (단정밀도) 실수로, f 붙여야 함
+            char d = 'A';
+
+
+            Console.Write("a:  {0}, a.GetType():  {1}\n", a, a.GetType());
+            Console.Write("b:  {0}, b.GetType():  {1}\n", b, b.GetType());
+            Console.Write("c:  {0}, c.GetType():  {1}\n", c, c.GetType());
+            Console.Write("d:  {0}, d.GetType():  {1}\n", d, d.GetType());
+
+        }
+    }
+}
