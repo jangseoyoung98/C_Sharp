01efb2cbb29b286642e320e39f4fda4f7acd474f
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-01-07 23:11:27 +0900

    01_055_Check

---
+++ [패캠] Prac_Check/055_Check.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _055_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 게임 만들기 : 숫자 달리기 1
+        }
+    }
+}
