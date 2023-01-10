7ecf81fdb418cedd3a3f6777d5758bf51a21de79
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-01-10 22:57:36 +0900

    01_056_Check

--- [패캠] Prac_Check/056_Check.cs
+++ [패캠] Prac_Check/056_Check.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// 랜덤한 정수(1~99)를 맞추는 프로그램
+using System;
 
 namespace _056_Check
 {
@@ -10,7 +7,40 @@ namespace _056_Check
     {
         static void Main(string[] args)
         {
-            // 게임 만들기 : 숫자 달리기 2
+            Random rnd = new Random();
+            int rndNum = rnd.Next(1, 100);
+
+            int input;
+            int i = 0;
+            
+            Console.Write("1~99 사이 어떤 숫자일까요~? (단, 0은 나가기) : ");
+
+            while (true)
+            {
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 0)
+                    break;
+                
+                i++;
+
+                if (rndNum == input)
+                {
+                    Console.WriteLine("== 축! 정답입니다. ==");
+                    break;
+                }
+                else if (rndNum > input)
+                {
+                    Console.WriteLine("흠..입력한 수는 작습니다!");
+                }
+                else // rndNum < input
+                {
+                    Console.WriteLine("흠..입력한 수는 큽니다!");
+                }
+            }
+
+            Console.WriteLine("총 {0}번 시도", i);
+
         }
     }
 }
